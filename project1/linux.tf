1a8b336ed4cd87ef09d71433512eaf888dd5de99
steph <stephane.clavel@sita.aero>
2020-06-03 12:30:44 +0200

    initial commit

---
+++ project1/linux.tf
@@ -0,0 +1,25 @@
+resource "vcd_vapp" "tf_linux_vapp" {
+  name      = "tf_linux_vapp"
+}
+
+resource "vcd_vapp_vm" "tf_linux_vm" {
+  vapp_name     = vcd_vapp.tf_linux_vapp.name
+  name          = "tf_linux_vm"
+  computer_name = "tf-linux-vm"
+  catalog_name  = var.src_catalog
+  template_name = var.src_template
+  power_on      = true
+
+  network {
+    type               = "org"
+    name               = var.org_net
+    ip_allocation_mode = "POOL"
+    is_primary         = true
+  }
+
+  customization {
+      enabled                   = true
+      auto_generate_password    = true
+  }
+}
+
---
+++ project1/terraform.tfvars
@@ -0,0 +1,8 @@
+vcd_user        = "orgadmin"
+vcd_pass        = "Nsetestbed09!"
+vcd_org         = "Test-ENG001"
+vcd_vdc         = "TEST-VDC1"
+vcd_site        = "fra"
+src_catalog     = "CAT_ANSIBLE"
+src_template    = "CentOS-7.7-64Bits"
+org_net   	= "ORG_NET_TEST_VDC1_NET2"
