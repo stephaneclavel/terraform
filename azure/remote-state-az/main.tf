d4c928189f38d8b118a5c7bbc85ccc75fd912023
steph <stephane.clavel@sita.aero>
2020-08-28 17:05:11 +0200

    first commit

---
+++ azure/remote-state-az/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tstate"
+    storage_account_name  = "tstate25079"
+    container_name        = "tstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+    version = "2.9.0"
+#    subscription_id = var.subscriptionID
+
+    features {}
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "eastus"
+}
---
+++ azure/remote-state-az/readme.md
@@ -0,0 +1,1 @@
+https://docs.microsoft.com/en-us/azure/developer/terraform/store-state-in-azure-storage
