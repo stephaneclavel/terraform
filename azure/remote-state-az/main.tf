b90f565d78276e523e5eb0f3eac3deb578cbdc76
steph <stephane.clavel@sita.aero>
2020-12-11 16:23:42 +0100

    tf

--- azure/remote-state-az/main.tf
+++ azure/remote-state-az/main.tf
@@ -1,24 +1,22 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "tstate"
-    storage_account_name  = "tstate25079"
-    container_name        = "tstate"
-    key                   = "terraform.tfstate"
+    resource_group_name  = "tstate"
+    storage_account_name = "tstate25079"
+    container_name       = "tstate"
+    key                  = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-    version = "2.9.0"
-#    subscription_id = var.subscriptionID
-
-    features {}
+  version = "2.9.0"
+  features {}
 }
 
 resource "azurerm_resource_group" "state-demo-secure" {
   name     = "state-demo"
-  location = "eastus"
+  location = "westeurope"
 
   tags = {
-    env = "az104"
+    env = "tf-demo"
   }
 }
