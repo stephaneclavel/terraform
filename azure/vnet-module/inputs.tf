6ab1a52eac6946d16821c2afc6e438ca235c4425
steph <stephane.clavel@sita.aero>
2020-12-11 17:52:19 +0100

    updated rg name

--- azure/vnet-module/inputs.tf
+++ azure/vnet-module/inputs.tf
@@ -2,14 +2,19 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "rg1"
-  location = "North Europe"
+resource "azurerm_resource_group" "rg-demo-test-westeurope-001" {
+  name     = "rg-demo-test-westeurope-001"
+  location = "westeurope"
+
+  tags = {
+    env = "tf-demo"
+  }
+
 }
 
 module "vnet" {
   source              = "Azure/vnet/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.rg-demo-test-westeurope-001.name
   address_space       = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
@@ -36,14 +41,14 @@ module "vnet" {
     costcenter  = "it"
   }
 
-  depends_on = [azurerm_resource_group.example]
+  depends_on = [azurerm_resource_group.rg-demo-test-westeurope-001]
 }
 
 resource "azurerm_network_security_group" "ssh" {
 #  depends_on          = [module.vnet]
   name                = "ssh"
-  location            = "North Europe"
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg-demo-test-westeurope-001.location
+  resource_group_name = azurerm_resource_group.rg-demo-test-westeurope-001.name
 
   security_rule {
     name                       = "test123"
@@ -60,14 +65,14 @@ resource "azurerm_network_security_group" "ssh" {
 }
 
 resource "azurerm_route_table" "example" {
-  location            = azurerm_resource_group.example.location
+  location            = azurerm_resource_group.rg-demo-test-westeurope-001.location
   name                = "MyRouteTable"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.rg-demo-test-westeurope-001.name
 }
 
 resource "azurerm_route" "example" {
   name                = "acceptanceTestRoute1"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.rg-demo-test-westeurope-001.name
   route_table_name    = azurerm_route_table.example.name
   address_prefix      = "10.1.0.0/16"
   next_hop_type       = "vnetlocal"
