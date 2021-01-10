c68620dc9b862ebc5d40c964063c041b456442bd
steph <stephane.clavel@sita.aero>
2021-01-10 11:25:29 +0100

    modified to deal with tf output adding quotes from 0.14

--- azure/vNET+VM/test-ssh.sh
+++ azure/vNET+VM/test-ssh.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-server=$(terraform output pip)      # server IP
+#server=$(terraform output pip)      # server IP
+server=$(terraform output -json | jq -r .pip.value)
 port=22                 # port
 connect_timeout=5       # Connection timeout
 
-ssh -q -o BatchMode=yes  -o StrictHostKeyChecking=no -o ConnectTimeout=$connect_timeout $server 'exit 0'
+ssh -q -o BatchMode=yes  -o StrictHostKeyChecking=no -o ConnectTimeout=$connect_timeout $server -p $port 'exit 0'
 if [ $? == 0 ];then
    echo "SSH Connection to $server over port $port is possible"
 else
