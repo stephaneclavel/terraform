7b97de89a11de85d49e10bbb3312f03eb5499415
steph <stephane.clavel@sita.aero>
2021-01-18 16:05:48 +0100

    simplified outputs

--- aws/MyFirstVPC/outputs.tf
+++ aws/MyFirstVPC/outputs.tf
@@ -1,8 +1,3 @@
-output "public_instance_public_ip" {
-  description = "Public instance public IP address"
-  value       = module.ec2_public.public_ip
-}
-
 output "public_instance_dns" {
   description = "Public instance dns"
   value       = module.ec2_public.public_dns
@@ -11,10 +6,5 @@ output "public_instance_dns" {
 output "private_instance_private_ip" {
   description = "Public instance public IP address"
   value       = module.ec2_private.private_ip
-}
-
-output "public_nat_instance_private_ip" {
-  description = "Public instance NAT private IP address"
-  value       = module.ec2_nat.private_ip
 }
 
