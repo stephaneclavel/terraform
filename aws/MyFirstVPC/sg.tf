058a79675216417d909d5567c8d63ffe7056251e
steph <stephane.clavel@sita.aero>
2021-01-18 11:03:53 +0100

    add outbound rule to SG

--- aws/MyFirstVPC/sg.tf
+++ aws/MyFirstVPC/sg.tf
@@ -5,20 +5,23 @@ data "http" "my_ip" {
 module "demo_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "allow-ssh"
+  name        = "allow-ssh-inbound-all-outbound"
   description = "Security group to allow inbound ssh from client ip"
   vpc_id      = module.base-network.vpc_id
 
   ingress_cidr_blocks = ["${data.http.my_ip.body}/32", module.base-network.public_subnet_cidr_blocks[0], module.base-network.public_subnet_cidr_blocks[1]]
-  #  ingress_rules            = ["https-443-tcp"]
   ingress_with_cidr_blocks = [
     {
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
       description = "ssh"
-      #cidr_blocks = format("%s/32", data.http.my_ip.body)
-      #cidr_blocks = "${data.http.my_ip.body}/32"
+    }
+  ]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_with_cidr_blocks = [
+    {
+      rule = "all-all"
     }
   ]
 }
