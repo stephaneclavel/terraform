b08b4b0e1db96be47c1b92512960164b321dcfcc
steph <stephane.clavel@sita.aero>
2021-02-22 17:44:29 +0100

    additional sg to limit traffic from lb to web servers

--- aws/04_asg_cloudwatch/sg.tf
+++ aws/04_asg_cloudwatch/sg.tf
@@ -50,3 +50,23 @@ module "unfiltered_http" {
   ]
 }
 
+module "http-from-lb-sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "unfiltered_http"
+  description = "SG to allow http from Internet"
+  vpc_id      = module.base-network.vpc_id
+
+  computed_ingress_with_source_security_group_id = [
+    {
+      from_port                = 80
+      to_port                  = 80
+      protocol                 = 6
+      description              = "http from LB"
+      source_security_group_id = module.unfiltered_http.this_security_group_id
+    }
+  ]
+
+  number_of_computed_ingress_with_source_security_group_id = 1
+
+}
