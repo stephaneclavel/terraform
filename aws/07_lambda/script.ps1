c07ee485b5bfb546bf9e1ce9716e9e359f5f372c
Steph <steph@windows.pc>
2021-03-20 20:05:20 +0100

    version is now a variable

--- aws/07_lambda/script.ps1
+++ aws/07_lambda/script.ps1
@@ -1,15 +1,21 @@
+$version = Read-Host -Prompt 'Input code version (1.0.0 or 1.0.1)'
 Set-Location .\a_s3_code\
+Write-Host "Creating S3 buckets" -ForegroundColor red
 terraform.exe apply -auto-approve
 $env:TF_VAR_bucket = terraform output bucket_name
 $env:TF_VAR_bucket = $env:TF_VAR_bucket.Trim('"')
-$env:TF_VAR_key = "v1.0.0/code.zip"
-aws s3 cp code100.zip s3://$env:TF_VAR_bucket/$env:TF_VAR_key
+$env:TF_VAR_key = "v$version/code.zip"
+Write-Host "Uploading code to bucket" -ForegroundColor red
+aws s3 cp code$version.zip s3://$env:TF_VAR_bucket/$env:TF_VAR_key
 
 Set-Location ..\b_lambda_api\
 
+Write-Host "Creating Lambda function and API gateway" -ForegroundColor red
 terraform.exe apply -auto-approve
 $url = terraform output base_url
 $url = $url.Trim('"')
-Invoke-WebRequest $url
+$content = Invoke-WebRequest $url
+Write-Host "Lambda function returned:" -ForegroundColor red
+$content.content
 
 Set-Location ..
