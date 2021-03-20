8b9eefd37e64ae9c624932aea0e8956c86df31ea
Steph <steph@windows.pc>
2021-03-20 18:58:30 +0100

    added lambda & api part

--- aws/07_lambda/script.ps1
+++ aws/07_lambda/script.ps1
@@ -1,9 +1,15 @@
 Set-Location .\a_s3_code\
 terraform.exe apply -auto-approve
-$output = terraform output bucket_name
-$key = "v1.0.0/code.zip"
-aws s3 cp code100.zip s3://$output/$key
+$env:TF_VAR_bucket = terraform output bucket_name
+$env:TF_VAR_bucket = $env:TF_VAR_bucket.Trim('"')
+$env:TF_VAR_key = "v1.0.0/code.zip"
+aws s3 cp code100.zip s3://$env:TF_VAR_bucket/$env:TF_VAR_key
 
 Set-Location ..\b_lambda_api\
+
+terraform.exe apply -auto-approve
+$url = terraform output base_url
+$url = $url.Trim('"')
+Invoke-WebRequest $url
 
 Set-Location ..
