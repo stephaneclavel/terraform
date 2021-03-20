992d42b16b42bfb11a2e9c1773ec4b15db4d8e48
Steph <steph@windows.pc>
2021-03-20 16:27:31 +0100

    changed name to code100

--- aws/07_lambda/script.ps1
+++ aws/07_lambda/script.ps1
@@ -1,5 +1,9 @@
 Set-Location .\a_s3_code\
 terraform.exe apply -auto-approve
 $output = terraform output bucket_name
-aws s3 cp code.zip s3://$output/v1.0.0/code.zip
+$key = "v1.0.0/code.zip"
+aws s3 cp code100.zip s3://$output/$key
 
+Set-Location ..\b_lambda_api\
+
+Set-Location ..
--- aws/07_lambda/script.sh
+++ aws/07_lambda/script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd .\a_s3_code\
+cd ./a_s3_code/
 terraform apply -auto-approve
 bucket=$(terraform output -json | jq -r .bucket_name.value)
-aws s3 cp code.zip s3://$output/v1.0.0/code.zip
+aws s3 cp code100.zip s3://$output/v1.0.0/code.zip
+
+cd ../b_lambda_api/
+
+cd ..
