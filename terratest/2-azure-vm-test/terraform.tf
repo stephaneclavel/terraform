e277d10536a8827855718ee25fe24f8555b51ed7
Stephane Clavel <stef@decidela.net>
2021-09-09 14:58:29 +0200

    first commit

---
+++ terratest/2-azure-vm-test/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tstate"
+    storage_account_name = "tstate250790"
+    container_name       = "terratest"
+    key                  = "2-azure-vm-test.tfstate"
+  }
+
+}
